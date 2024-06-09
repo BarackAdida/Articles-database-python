2c41c9c6098246ec78c6b044351bb6e8986545aa
Adida <adida.barack@student.moringaschool.com>
2024-06-09 11:24:59 +0300

    Almost done code

--- models/Magazine.py
+++ models/Magazine.py
@@ -1,22 +1,33 @@
+from .connection import db
+
 class Magazine:
     def __init__(self, id, name, category):
         self._id = id
         self._name = name
         self._category = category
 
-    @property
-    def id(self):
-        return self._id
+    def article_titles(self):
+        query = """
+        SELECT title FROM Article WHERE magazine_id = :magazine_id
+        """
+        with db.connect() as conn:
+            result = conn.execute(query, magazine_id=self.id)
+            titles = [row[0] for row in result.fetchall()]
+            return titles
 
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def category(self):
-        return self._category
-
-    @classmethod
-    def from_database(cls, magazine_id):
-        # Implement a method to retrieve magazine details from the database
-        pass
+    def contributing_authors(self):
+        query = """
+        SELECT a.name
+        FROM Author a
+        JOIN (
+            SELECT author_id, COUNT(*) as article_count
+            FROM Article
+            WHERE magazine_id = :magazine_id
+            GROUP BY author_id
+            HAVING COUNT(*) > 2
+        ) AS ac ON a.id = ac.author_id
+        """
+        with db.connect() as conn:
+            result = conn.execute(query, magazine_id=self.id)
+            authors = [row[0] for row in result.fetchall()]
+            return authors
